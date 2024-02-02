0756e5c11c01ddafc7ee47eda95665fbf338e64c
KIMSEONGJUNG <qpwo10@g.skku.edu>
2024-02-03 03:39:09 +0900

    :art: Update dependencies and remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_comit_study_scheduler_app/view/home_page.dart';
-import 'package:flutter_comit_study_scheduler_app/view/login_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_comit_study_scheduler_app/view/register_page.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+// import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/route_manager.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   KakaoSdk.init(nativeAppKey: 'f2ad9647c357bde4bf6e159a4f10ec61');
-  // runApp(MyApp());
-  runApp(PlatformApp(
-    home: MyApp(),
-  ));
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,14 +22,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(393, 852), // iPhone 14 Pro
-      builder: (context, child) => MaterialApp(
+      builder: (context, child) => GetMaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Study Scheduler',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        // home: HomePage(),
-        // home: LoginPage(),
         home: RegisterPage(),
       ),
     );
