3386939c7a46d519f8b25093c559a3206f6ec8f7
KIMSEONGJUNG <qpwo10@g.skku.edu>
2024-02-03 04:31:36 +0900

    :art: Refactor register_page.dart

--- lib/view/register_page.dart
+++ lib/view/register_page.dart
@@ -3,106 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
-  void kakaologin() async {
-    if (await isKakaoTalkInstalled()) {
-      try {
-        await UserApi.instance.loginWithKakaoTalk();
-        print('카카오톡으로 로그인 성공!');
-      } catch (error) {
-        print(error);
-
-        if (error is PlatformException && error.code == 'CANCELED') {
-          return;
-        }
-        try {
-          await UserApi.instance.loginWithKakaoAccount();
-          print('카카오계정으로 로그인 성공!');
-        } catch (error) {
-          print(error);
-        }
-      }
-    } else {
-      print('카카오톡이 설치되어 있지 않습니다.');
-      try {
-        await UserApi.instance.loginWithKakaoAccount();
-        print('카카오계정으로 로그인 성공!');
-      } catch (error) {
-        print('카카오계정으로 로그인 실패 $error');
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Study Scheduler',
-              style: TextStyle(
-                fontSize: 30.sp,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20.h),
-            TextField(
-              decoration: InputDecoration(
-                hintText: '이메일',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-            SizedBox(height: 20.h),
-            TextField(
-              decoration: InputDecoration(
-                hintText: '비밀번호',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-            SizedBox(height: 20.h),
-            InkWell(
-              onTap: () => kakaologin(),
-              child: Ink(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Color(0XFFFEE500),
-                ),
-                width: double.infinity,
-                height: 50.h,
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Row(
-                    children: [
-                      Image(
-                        image: AssetImage(
-                          'assets/images/kakao_logo.svg',
-                        ),
-                      ),
-                      Text('카카오 로그인',
-                          style: TextStyle(
-                            fontSize: 20.sp,
-                            fontWeight: FontWeight.bold,
-                          )),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
