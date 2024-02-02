4694af96e107dbbbfe833648a4b9e1a0ad73279a
KIMSEONGJUNG <qpwo10@g.skku.edu>
2024-02-03 03:39:31 +0900

    :bug: Update register_page.dart with Kakao login functionality

--- lib/view/register_page.dart
+++ lib/view/register_page.dart
@@ -8,16 +8,46 @@ import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
+  void kakaologin() async {
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공!');
+      } catch (error) {
+        print(error);
+
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공!');
+        } catch (error) {
+          print(error);
+        }
+      }
+    } else {
+      print('카카오톡이 설치되어 있지 않습니다.');
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공!');
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: double.infinity,
         padding: EdgeInsets.symmetric(horizontal: 20.w),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              '회원가입',
+              'Study Scheduler',
               style: TextStyle(
                 fontSize: 30.sp,
                 fontWeight: FontWeight.bold,
@@ -26,60 +56,49 @@ class RegisterPage extends StatelessWidget {
             SizedBox(height: 20.h),
             TextField(
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '아이디',
+                hintText: '이메일',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
             SizedBox(height: 20.h),
             TextField(
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '비밀번호',
+                hintText: '비밀번호',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
-              obscureText: true,
             ),
             SizedBox(height: 20.h),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '비밀번호 확인',
+            InkWell(
+              onTap: () => kakaologin(),
+              child: Ink(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0XFFFEE500),
+                ),
+                width: double.infinity,
+                height: 50.h,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Image(
+                        image: AssetImage(
+                          'assets/images/kakao_logo.svg',
+                        ),
+                      ),
+                      Text('카카오 로그인',
+                          style: TextStyle(
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ],
+                  ),
+                ),
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20.h),
-            ElevatedButton(
-              onPressed: () async {
-                if (await isKakaoTalkInstalled()) {
-                  try {
-                    await UserApi.instance.loginWithKakaoTalk();
-                  } catch (error) {
-                    print(error);
-
-                    if (error is PlatformException &&
-                        error.code == 'CANCELED') {
-                      return;
-                    }
-                    try {
-                      await UserApi.instance.loginWithKakaoAccount();
-                      print('카카오계정으로 로그인 성공!');
-                    } catch (error) {
-                      print(error);
-                    }
-                  }
-                } else {
-                  try {
-                    await UserApi.instance.loginWithKakaoAccount();
-                    print('카카오계정으로 로그인 성공!');
-                  } catch (error) {
-                    print('카카오계정으로 로그인 실패 $error');
-                  }
-                }
-              },
-              child: Text('카카오 로그인'),
-            ),
-            PlatformElevatedButton(
-              child: Text('login'),
-              onPressed: () {},
             ),
           ],
         ),
