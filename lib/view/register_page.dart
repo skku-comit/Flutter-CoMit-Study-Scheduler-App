779d3c3c42d7d193157f0aa2e8063abb7fb422c9
KIMSEONGJUNG <qpwo10@g.skku.edu>
2024-02-02 14:19:49 +0900

    :sparkles: Add Kakao login functionality

--- lib/view/register_page.dart
+++ lib/view/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -48,6 +49,30 @@ class RegisterPage extends StatelessWidget {
               child: Text('회원가입'),
             ),
             SizedBox(height: 20.h),
+            ElevatedButton(
+              onPressed: () async {
+                if (await isKakaoTalkInstalled()) {
+                  try {
+                    await UserApi.instance.loginWithKakaoTalk();
+                  } catch (error) {
+                    print(error);
+
+                    try {
+                      await UserApi.instance.loginWithKakaoAccount();
+                    } catch (error) {
+                      print(error);
+                    }
+                  }
+                } else {
+                  try {
+                    await UserApi.instance.loginWithKakaoAccount();
+                  } catch (error) {
+                    print(error);
+                  }
+                }
+              },
+              child: Text('카카오 로그인'),
+            ),
           ],
         ),
       ),
