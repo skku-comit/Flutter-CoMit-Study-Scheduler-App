8d0f56156bebaf27621d635bbca923918f4a7c6f
KIMSEONGJUNG <qpwo10@g.skku.edu>
2024-02-04 22:45:01 +0900

    :sparkles: Add EventModel class

---
+++ lib/model/event_model.dart
@@ -0,0 +1,13 @@
+class EventModel {
+  final String title;
+  final String description;
+  final String date;
+  final String time;
+
+  EventModel({
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.time,
+  });
+}
