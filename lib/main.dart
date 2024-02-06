cb07dfa9d4885275a040ddda174f9875655c50fd
KIMSEONGJUNG <qpwo10@g.skku.edu>
2024-02-06 14:25:55 +0900

    :sparkles: Add dotenv and Supabase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,27 @@ import 'package:flutter_comit_study_scheduler_app/view/profile_page.dart';
 import 'package:flutter_comit_study_scheduler_app/view/home_page.dart';
 import 'package:flutter_comit_study_scheduler_app/view/login_page.dart';
 import 'package:flutter_comit_study_scheduler_app/view/register_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 // import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 // import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/route_manager.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  KakaoSdk.init(nativeAppKey: 'f2ad9647c357bde4bf6e159a4f10ec61');
+  await dotenv.load(fileName: ".env");
+
+  KakaoSdk.init(
+      nativeAppKey: dotenv.get('KAKAO_NATIVE_APP_KEY')); // Kakao App Key
+
+  await Supabase.initialize(
+    url: dotenv.get('SUPABASE_URL'),
+    anonKey: dotenv.get('SUPABASE_ANON_KEY'),
+  );
+
   initializeDateFormatting().then((_) => runApp(MyApp()));
 }
 
