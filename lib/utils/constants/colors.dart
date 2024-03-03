9b259102218a6a24296fc4015fcc34080fc3b8cf
KIMSEONGJUNG <qpwo10@g.skku.edu>
2024-03-03 21:56:01 +0900

    :lipstick: Add AppColors class with color constants

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color study = Colors.lightBlue;
+  static const Color meeting = Colors.purple;
+  static const Color etc = Colors.teal;
+
+  // static const Color studyBackground = Color(0xFFE6E6FA);
+  // static Color meetingBackground = Color(0xFFE6E6FA);
+  // static const Color etcBackground = Color(0xFFE0FFFF);
+  static const Color studyBackground = Colors.white;
+  static const Color meetingBackground = Colors.white;
+  static const Color etcBackground = Colors.white;
+}
