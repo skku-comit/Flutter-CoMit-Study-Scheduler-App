435d1ad51644b1a53eefa16c8ae9fe4fe3d2c279
KIMSEONGJUNG <qpwo10@g.skku.edu>
2024-03-09 18:49:00 +0900

    :art: Add formatHour and formatMinute methods to Formatter class

--- lib/utils/formatters/formatter.dart
+++ lib/utils/formatters/formatter.dart
@@ -10,4 +10,14 @@ class Formatter {
     time ??= DateTime.now(); // time이 null이면 현재 시간으로 설정
     return DateFormat('HH:mm').format(time);
   }
+
+  static String formatHour(int? hour) {
+    hour ??= 0; // hour가 null이면 0으로 설정
+    return hour.toString().padLeft(2, '0');
+  }
+
+  static String formatMinute(int? minute) {
+    minute ??= 0; // minute이 null이면 0으로 설정
+    return minute.toString().padLeft(2, '0');
+  }
 }
